32fb47a3cb8956d61c41a491838a5bff11f3a05d
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2025-07-09 00:03:18 +0600

    variables

---
+++ variables/src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        /// integer
+        int age = 24;
+        int year=2025;
+
+        System.out.println(age);
+
+        double  cgpa=3.40;
+        double temperature=23.5;
+        System.out.println(cgpa);
+
+        char grade ='A';
+        char symbol='!';
+        char currency='$';
+        System.out.println(symbol);
+
+        boolean isStudent =true;
+        boolean forSale=false;
+
+        if(isStudent){
+            System.out.println("You are a student!");
+
+        }
+        else{
+            System.out.println("You are NOT a student");
+
+        }
+
+        /// Reference variable
+        String name="Easin Khandokar";
+        String food="pizza";
+        String email="easin@gmail.com";
+
+        System.out.println("Your email is "+email);
+        System.out.println("You are "+age +" years old");
+        System.out.println("Your average grade "+cgpa +" cgpa");
+
+
+
+
+
+    }
+
+
+}
