dbb543a19ff1b554aa92bcc3b28062d735b40d83
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2025-07-09 12:45:02 +0600

    arithmetic

---
+++ Arithmetic/src/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+
+
+    public static void main (String []args){
+/*
+        int x=10;
+        int y=3;
+        int z;
+*/
+        //z=x-y;
+        //z=x+y;
+        //z=x*y;
+        //z=x/y;
+        //z=x%y;
+
+        ///increment /decrement operator
+        int x=1;
+
+        ++x;
+
+        x++;
+        x++;
+
+
+        System.out.println(x);
+
+
+        int y=1;
+        --y;
+        y--;
+        y++;
+        y++;
+
+        System.out.println(y);
+
+
+        /// Oder of Operation [P-E-M-D-A-S]
+        double result =3+4*(7-5)/2.0;
+        System.out.println(result);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
