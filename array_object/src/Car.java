ba1cbced6026a5c09af71d42f3bd41b4c572906f
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2025-08-21 20:29:15 +0600

    array object

---
+++ array_object/src/Car.java
@@ -0,0 +1,20 @@
+public class Car {
+
+    String model;
+    String color;
+
+    Car(String model, String color){
+        this.model= model;
+        this.model= color;
+
+    }
+
+    void drive(){
+        System.out.println("You drive the "+ this.color+ " "+ this.model);
+    }
+
+
+
+
+
+}
---
+++ array_object/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+
+public static void main(String [] args){
+
+//    Car car1= new Car("Mustang", "Red");
+//    Car car2= new Car("Corvette", "Blue");
+//    Car car3= new Car("Charger", "Yellow");
+//
+//    Car[] cars={car1, car2, car3};
+
+//    for(int i=0;i<cars.length ;i++)            {
+//        cars[i].drive();
+//    }
+
+
+    Car[] cars={new Car("Mustang", "Red"),
+            new Car("Corvette", "Blue"),
+            new Car("Charger", "Yellow")};
+
+for(Car car : cars){
+    car.color="black";
+}
+
+for(Car car : cars){
+    car.drive();
+}
+
+
+}
+
+
+
+
+
+}
